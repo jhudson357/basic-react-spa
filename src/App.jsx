a40dda4764069e293fbee405855901f9e343f21c
James Hudson <jmh357@comcast.net>
2023-03-14 09:49:56 -0500

    add dynmaic Links

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Products from './pages/Products'
 import RootLayout from './pages/RootLayout'
 import ErrorPage from './pages/Error'
+import ProductDetail from './pages/ProductDetail'
 
 const router = createBrowserRouter([
   { 
@@ -12,7 +13,8 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />, 
     children: [
       { path: '/', element: <Home /> },
-      { path: '/products', element: <Products /> }
+      { path: '/products', element: <Products /> },
+      { path: '/products/:productId', element: <ProductDetail /> },
     ]
   },
 ])
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,25 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 const Home = () => {
+  const navigate = useNavigate()
+
+  function navigateHandler() {
+    navigate('/products')
+  }
+  
   return (
     <>
       <h1>My Home Page</h1>
       <p>
         Go to <Link to='/products'>the list of products.</Link>
       </p>
+      {/* Should not create buttons and then navigate programatically (Navigate button below). You could just use link (Products button below). Example is to show how useNavigate works */}
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
+      </p>
+      {/* <Link to='/products'>
+        <button>Products</button>
+      </Link> */}
     </>
   )
 }
